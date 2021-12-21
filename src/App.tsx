a2af44651e6a81ded38aac33a2b8338023a5076b
Stefan Gregor <stefan.gregor@mantro.net>
2021-12-21 14:23:58 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <h1 className="text-5xl font-bold underline">
+            Finally with tailwind
+          </h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
