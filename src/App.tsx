03befcf89516613f78d4b703fb83af605d1cc3f2
Stefan Gregor <stefan.gregor@mantro.net>
2021-12-22 15:53:40 +0100

    [ENH] More routing tests

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,55 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 import { TopNavigation, ITopNavigationItem } from './components/navigation/TopNavigation';
 import { Content } from './components/main/Content';
 import { Input } from './components/main/Input';
+import { FormEvent, useState } from 'react';
+
+const initialRoutes: ITopNavigationItem[] = [
+  {
+    name: 'Dahoam',
+    to: '/dahoam'
+  },
+  {
+    name: 'Noch Daheimer',
+    to: '/mehr_dahoam'
+  }
+];
 
 function App() {
 
+  const [routes, setRoutes] = useState(initialRoutes);
+
   const navigate = useNavigate();
 
-  const menuItems: ITopNavigationItem[] = [
-    {
-      name: 'Dahoam',
-      to: '/dahoam'
-    },
-    {
-      name: 'Noch Daheimer',
-      to: '/mehr_dahoam'
-    },
-    {
-      name: 'Am Daheimsten',
-      to: '/am_daheimsten/Am Daheimsten vom Allerfeinsten'
+  function onInput(event: FormEvent<HTMLInputElement>) {
+    const value = event.currentTarget.value;
+    if (!value) {
+      const routes = initialRoutes.slice();
+      setRoutes(routes);
+      navigate(`/geheime_route/`);
+      return;
     }
-  ];
+    const routes = initialRoutes.slice();
+    routes.push({
+      name: value,
+      to: `/am_daheimsten/${value}`
+    });
+    setRoutes(routes);
+    navigate(`/geheime_route/${event.currentTarget.value}`);
+  }
 
   return (
     <div className="flex flex-col h-screen bg-cover bg-main">
       <header className="bg-slate-800">
-        <TopNavigation items={menuItems} />
+        <TopNavigation items={routes} />
       </header>
       <main className='flex flex-col items-center h-screen'>
         <Routes>
           <Route path="/dahoam" element={<Content name={"Daheim"} />} />
           <Route path="/mehr_dahoam" element={<Content name={"Mehr Daheim"} />} />
-          <Route path="/am_daheimsten" element={<Content name={"Ohne Parameter mach ich keinen Meter"} />} />
-          <Route path="/am_daheimsten/:wie_viel_daheim_parameter_id" element={<Content name={""} />} />
+          <Route path="/geheime_route" element={<Content name={"Ohne Parameter mach ich keinen Meter"} />} />
+          <Route path="/geheime_route/:wie_viel_geheim_parameter_id" element={<Content name={""} />} />
         </Routes>
-        <Input onInput={(event) => navigate(`/am_daheimsten/${event.currentTarget.value}`)} />
+        <Input onInput={onInput} />
       </main>
       <footer className="bg-slate-800">
         <h1 className="p-8 hover:uppercase text-white text-center">
--- src/components/main/Content.tsx
+++ src/components/main/Content.tsx
@@ -2,11 +2,11 @@ import { useParams } from "react-router-dom";
 
 export function Content({ name }: { name: string }) {
 
-    const { wie_viel_daheim_parameter_id } = useParams();
+    const { wie_viel_geheim_parameter_id } = useParams();
 
     return <div className="inline-flex self-center m-10 bg-pink-600 text-white rounded-full mx-20 px-12 py-8 hover:blur shadow-xl">
         <h1 className="text-4xl font-bold text-center">
-            {`${name}${wie_viel_daheim_parameter_id ? ` ${wie_viel_daheim_parameter_id}` : ''}`}
+            {`${name}${wie_viel_geheim_parameter_id ? ` ${wie_viel_geheim_parameter_id}` : ''}`}
         </h1>
     </div>
 }
