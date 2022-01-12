7d891966e179bc32ed18661b83768760160f4087
Stefan Gregor <stefan.gregor@mantro.net>
2022-01-12 12:41:49 +0100

    [ENH] Added text shadow

--- src/components/main/Content.tsx
+++ src/components/main/Content.tsx
@@ -4,7 +4,7 @@ export function Content({ value, user }: { value?: string, user?: string }) {
 
   const { wie_viel_geheim_parameter_id } = useParams();
 
-  return <div className="text-white self-center max-w-[50%] my-32 hover:opacity-80" >
+  return <div className="text-white text-shadow self-center max-w-[50%] my-32 hover:opacity-80" >
     {value &&
       <div className="mobile:text-2xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-left">
         {value}
