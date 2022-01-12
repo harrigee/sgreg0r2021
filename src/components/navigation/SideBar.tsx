04ee88e568689e893aa1d7e53c27449dd4c8e6ff
Stefan Gregor <stefan.gregor@mantro.net>
2022-01-12 14:57:24 +0100

    [ENH] ui changes

--- src/components/navigation/SideBar.tsx
+++ src/components/navigation/SideBar.tsx
@@ -1,5 +1,5 @@
 export function SideBar({ items }: { items: { uid: string, displayName: string, isOnline: boolean }[] }) {
-  return <div className="w-8 bg-green-400 transition-all ease-in-out duration-500 hover:w-80 hover:bg-zinc-800 pt-8 overflow-y-scroll">
+  return <div className="w-8 bg-green-400 transition-all ease-in-out duration-500 hover:w-80 hover:bg-zinc-800 pt-8 overflow-y-scroll overflow-x-hidden">
     <div>
       {items.map((item, i) =>
         <div
@@ -7,14 +7,14 @@ export function SideBar({ items }: { items: { uid: string, displayName: string, 
           key={item.displayName + i}>
           <div className={`flex p-2 rounded-full bg-zinc-600 border-4 ${item.isOnline ? 'border-green-400' : 'border-pink-500'}`}>
             <img
-              width={32}
-              height={32}
+              width={48}
+              height={48}
               alt="avatar"
-              src={`https://avatars.dicebear.com/api/human/${item.uid}.svg`} />
+              src={`https://avatars.dicebear.com/api/avataaars/${item.uid}.svg`} />
           </div>
-          <p className="text-white text-left self-center ml-4">
+          <h1 className="text-white font-medium text-left self-center ml-4">
             {item.displayName}
-          </p>
+          </h1>
         </div>
       )}
     </div>
