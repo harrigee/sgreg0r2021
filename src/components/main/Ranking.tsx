31bf666ad4ac7911f22f4778ceb877400edf5254
Stefan Gregor <stefan.gregor@mantro.net>
2022-01-13 13:49:29 +0100

    [ENH] Design changes

--- src/components/main/Ranking.tsx
+++ src/components/main/Ranking.tsx
@@ -8,17 +8,17 @@ export function Ranking({ items, type }: { items: { uid: string, displayName: st
         <div
           className="flex flex-shrink-0 w-36 mx-4 my-4 items-center content-center hover:animate-pulse"
           key={item.displayName + i}>
-          <div className={`flex-shrink-0 relative p-2 justify-center rounded-full bg-zinc-600 border-4 ${item.isOnline ? 'border-green-400' : 'border-pink-500'}`}>
+          <div className={`flex-shrink-0 relative p-1 justify-center rounded-full bg-zinc-600 border-4 ${item.isOnline ? 'border-green-400' : 'border-pink-500'}`}>
             {i === 0 &&
-              <div className="absolute -translate-y-3 translate-x-2 rotate-12">
-                <FaCrown size={16} color='yellow' />
+              <div className="absolute -translate-y-4 translate-x-3 rotate-12">
+                <FaCrown size={28} color='yellow' />
               </div>
             }
             <img
-              width={i === 0 ? 32 : i === 1 ? 24 : 16}
-              height={i === 0 ? 32 : i === 1 ? 24 : 16}
+              width={40}
+              height={40}
               alt="avatar"
-              src={`https://avatars.dicebear.com/api/avataaars/${item.uid}.svg`} />
+              src={`https://avatars.dicebear.com/api/adventurer/${item.uid}.svg`} />
           </div>
           <div className="flex flex-col self-center">
             <p className="text-white text-sm text-left ml-4">
@@ -41,19 +41,19 @@ export function Ranking({ items, type }: { items: { uid: string, displayName: st
   return <div className="w-8 bg-green-400 shadow-2xl scrollbar-hide transition-all ease-in-out duration-500 hover:w-80 hover:bg-zinc-800 pt-16 overflow-y-scroll overflow-x-hidden">
     {items.map((item, i) =>
       <div
-        className="flex w-80 items-center content-center py-4 px-8 hover:animate-pulse"
+        className="flex w-80 items-center content-center py-5 px-8 hover:animate-pulse"
         key={item.displayName + i}>
-        <div className={`relative p-2 rounded-full bg-zinc-600 border-4 ${item.isOnline ? 'border-green-400' : 'border-pink-500'}`}>
+        <div className={`relative p-1 rounded-full bg-zinc-600 border-4 ${item.isOnline ? 'border-green-400' : 'border-pink-500'}`}>
           {i === 0 &&
-            <div className="absolute -translate-y-5 translate-x-5 rotate-12">
-              <FaCrown size={40} color='yellow' />
+            <div className="absolute -translate-y-5 translate-x-8 rotate-12">
+              <FaCrown size={48} color='yellow' />
             </div>
           }
           <img
-            width={i === 0 ? 64 : i === 1 ? 48 : 24}
-            height={i === 0 ? 64 : i === 1 ? 48 : 24}
+            width={i === 0 ? 92 : i === 1 ? 72 : 48}
+            height={i === 0 ? 92 : i === 1 ? 72 : 48}
             alt="avatar"
-            src={`https://avatars.dicebear.com/api/avataaars/${item.uid}.svg`} />
+            src={`https://avatars.dicebear.com/api/adventurer/${item.uid}.svg`} />
         </div>
         <div className="flex flex-col self-center grow">
           <p className="text-white text-m text-left ml-4">
