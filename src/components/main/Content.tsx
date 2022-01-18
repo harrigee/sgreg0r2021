b7d55b5ac23f3fac990f5cf1736579de98d11b69
Stefan Gregor <stefan.gregor@mantro.net>
2022-01-18 08:26:28 +0100

    [ENH] Added linkify

--- src/components/main/Content.tsx
+++ src/components/main/Content.tsx
@@ -1,9 +1,11 @@
+import Linkify from "react-linkify";
+
 export function Content({ value, user }: { value?: string; user?: string }) {
   const text = value && value !== "" ? value : "Just write...";
   return (
     <div className="text-white text-shadow self-center max-w-[50%] my-16 hover:opacity-80">
       <p className="break-words text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-left">
-        {text}
+        <Linkify>{text}</Linkify>
       </p>
       {user && <p className="mt-4">{user}</p>}
     </div>
